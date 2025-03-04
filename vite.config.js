eeef1efb817158b08ca997f6321cf004d2ad2650
Shashidharak89 <shashidharak334@gmail.com>
2025-03-04 23:18:41 +0530

    add vercel json

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,28 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5173,
+    open: true,
+    historyApiFallback: true, // Ensures React Router works on refresh
+  },
   resolve: {
     alias: {
       'three': 'three'
     }
   }
 })
+
+
+
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// export default defineConfig({
+//     plugins: [react()],
+//     server: {
+//         port: 5173,
+//         open: true,
+//         historyApiFallback: true, // Ensures React Router works on refresh
+//     },
+// });
